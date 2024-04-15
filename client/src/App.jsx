7baabad0cc884f08af2a4ed0688efbce5aa51af8
stephenlit <stevish45@icloud.com>
2024-04-14 23:40:45 -0300

    Added Layout to routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import  {Home, About, Contact, Services}  from './components/pages/index';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Contact, Services } from "./components/pages/index";
+import { Layout } from "./components/pages/layouts/layout";
 
 function App() {
-    return (
-        <div className='App'>
-            <Navbar />
-            <Routes>
-                <Route path='/about' element={<About />} />
-                <Route path='/services' element={<Services />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/' element={<Home />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
